6832d66ea5a54b99efc9c28348de2f57e565a62e
cucu0628 <adambence2007@gmail.com>
2025-02-14 09:13:32 +0100

    sem

--- Untitled-1.py
+++ Untitled-1.py
@@ -83,23 +83,30 @@ def jegyfoglalas_ablak(terem_szam, film_cim, szabad_helyek, frissit_film_lista):
         keresztnev = keresztnev_entry.get()
         vezeteknev = vezeteknev_entry.get()
         helyek_szama = jegy_szam.get()
-        if keresztnev and vezeteknev:
-            c.execute("SELECT szek_szam FROM foglalasok WHERE terem_szam = ?", (terem_szam,))
-            foglalt_helyek = {row[0] for row in c.fetchall()}
-            sikeres = False
-            for i in range(1, szabad_helyek + 1):
-                if i not in foglalt_helyek and helyek_szama > 0:
-                    if uj_foglalas(keresztnev, vezeteknev, terem_szam, i):
-                        helyek_szama -= 1
-                        sikeres = True
-            if sikeres:
-                messagebox.showinfo("Siker", "Foglalás sikeres!")
-                foglalas_window.destroy()
-                frissit_film_lista()
+        jegy = jegy_szam.get()
+        if szabad_helyek+1 > jegy:
+            if keresztnev and vezeteknev:
+                c.execute("SELECT szek_szam FROM foglalasok WHERE terem_szam = ?", (terem_szam,))
+                foglalt_helyek = {row[0] for row in c.fetchall()}
+                sikeres = False
+                for i in range(1, szabad_helyek + 1):
+                    if i not in foglalt_helyek and helyek_szama > 0:
+                        if uj_foglalas(keresztnev, vezeteknev, terem_szam, i):
+                            if helyek_szama < 0:
+                                messagebox.showerror("Hiba", "Nincs elegendő hely!")
+                            else:
+                                helyek_szama -= 1
+                                sikeres = True
+                if sikeres:
+                    messagebox.showinfo("Siker", "Foglalás sikeres!")
+                    foglalas_window.destroy()
+                    frissit_film_lista()
+                else:
+                    messagebox.showerror("Hiba", "Nem sikerült a foglalás!")
             else:
-                messagebox.showerror("Hiba", "Nem sikerült a foglalás!")
+                messagebox.showerror("Hiba", "Minden mezőt ki kell tölteni!")
         else:
-            messagebox.showerror("Hiba", "Minden mezőt ki kell tölteni!")
+            messagebox.showerror("Hiba","A foglalni kívánt jegyszám meghaladja a hátralévő helyek számát")
 
     Button(foglalas_window, text="Foglalás", command=foglalas).pack(pady=10)
 
