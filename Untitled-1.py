460fc7cd4ec918a40164d2d583327cb905c22135
cucu0628 <adambence2007@gmail.com>
2025-03-07 07:44:42 +0100

    abalak meret

--- Untitled-1.py
+++ Untitled-1.py
@@ -172,7 +172,7 @@ def jegyek_listazasa(frissit_film_lista):
     
     jegyek_window = Toplevel()
     jegyek_window.title("Vásárolt Jegyek")
-    jegyek_window.geometry("500x400")
+    jegyek_window.geometry("1100x400")
     
     Label(jegyek_window, text="Vásárolt Jegyek", font=("Arial", 14)).pack(pady=5)
     
